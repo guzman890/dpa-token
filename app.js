6c61921b4ce1e5b9e390e968e99cbe50e88d9f7b
guzman890 <guzmanfernando890@gmail.com>
2017-07-13 13:58:21 -0500

    add luma code

--- app.js
+++ app.js
@@ -1,46 +1,65 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+require('./app_api/models/db');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+
+var routesApi = require('./app_api/routes/index');//VARIABLE PARA LAS RUTAS DE LA AP
+
+
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
 
-// catch 404 and forward to error handler
+
+app.use('/api', routesApi);//ESPECIFICA QUE SE USARA la API PARA LAS PETICIONES GET POST DELETE
+
+/// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handler
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
+
 
 module.exports = app;
---
+++ app_api/routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+var ctrlUser= require('../controllers/user');
+
+/*User api*/
+router.get('/users', ctrlUser.UserListById);//obtenemos la lista de usuarios
+router.post('/users', ctrlUser.UserCreate);//crear user
+router.get('/users/:userid', ctrlUser.UserReadOne);//mosrar un user en especifico
+router.put('/users/:userid', ctrlUser.UserUpdateOne);// actualizar un user en especifico
+router.delete('/users/:userid', ctrlUser.UserDeleteOne); //eliminar un user en especifico
+
+module.exports = router;
