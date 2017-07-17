e05ced81a794459887ed9b61b868b2cad5d489ee
guzman890 <guzmanfernando890@gmail.com>
2017-07-16 21:47:43 -0500

    add_api

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/api', routesApi);//ESPECIFICA QUE SE USARA la API PARA LAS PETICIONES GET POST DELETE
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
